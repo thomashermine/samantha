015fe5c0fd95661ca08caa677706bab692ae9fea
Thomas Hermine <hello@thomashermine.be>
2023-10-19 13:52:06 +0200

    feat(app): Output process.env

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import 'dotenv/config';
 
 async function main() {
   console.log("Hello, world!");
+  console.log('Here are your environnment variables:');
+  console.log(process.env);
 }
 
 main();
